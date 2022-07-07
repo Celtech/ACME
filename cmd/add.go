5bf3ff1bf102c1d1aacf2aac6b16a57760195617
Tim Hinz <tim@chargeover.com>
2022-07-07 15:39:34 -0500

    chore: update `add` cli command long desc and add example usage

--- cmd/add.go
+++ cmd/add.go
@@ -12,7 +12,18 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add [username] [password]",
 	Short: "Adds a new API authorized user to the database",
-	Long:  "Adds a new API authorized user to the database",
+	Long: `Adds a new API authorized user to the database.
+This user will be able to fetch a access token from
+the API and authorize to ALL api endpoints.
+
+This command accepts a plain text password and will
+hash it before writing to the database.
+
+Username is preferred to be an email but anything may
+be used. Note when using the API, this will be referred
+to as an email regardless of whether you used an email
+or not.`,
+	Example: "baker-acme add support@example.com mySuperSecurePassword123",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 2 {
 			log.Error("username or password argument is missing")
