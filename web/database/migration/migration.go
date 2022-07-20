fa140b682402157c7998c4d0fee6c09cf300da08
Tim Hinz <tim@chargeover.com>
2022-07-20 12:32:11 -0500

    refactor: auto migrate certificates

--- web/database/migration/migration.go
+++ web/database/migration/migration.go
@@ -3,11 +3,17 @@ package migration
 import (
 	"github.com/Celtech/ACME/web/database"
 	"github.com/Celtech/ACME/web/model"
+	log "github.com/sirupsen/logrus"
 )
 
 func RunMigrations() {
-	database.GetDB().AutoMigrate(
+	err := database.GetDB().AutoMigrate(
+		&model.Certificate{},
 		&model.Request{},
 		&model.User{},
 	)
+
+	if err != nil {
+		log.Errorf("There was a problem executing database migrations: %v", err)
+	}
 }
