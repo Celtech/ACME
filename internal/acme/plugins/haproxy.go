4bca312621f212d211f4306e5ac11ffc5ff200f8
Tim Hinz <tim@chargeover.com>
2023-10-06 09:59:12 -0500

    refactor: haproxy mutli server plugin support

--- internal/acme/plugins/haproxy.go
+++ internal/acme/plugins/haproxy.go
@@ -4,38 +4,56 @@ import (
 	"fmt"
 	acmeConfig "github.com/Celtech/ACME/config"
 	tcp_socket "github.com/Celtech/ACME/internal/tcp-socket"
+	log "github.com/sirupsen/logrus"
 	"strings"
 )
 
+type HAProxyServer struct {
+	Host string
+	Port int
+}
+
+type HaProxyPlugin struct {
+	Enable  bool
+	Servers []HAProxyServer
+}
+
 func Run(certListPath string, certPath string, contents string) error {
-	enable := acmeConfig.GetConfig().GetBool("plugins.haproxy.enable")
-	if enable != true {
+	var haProxyPlugin HaProxyPlugin
+	err := acmeConfig.GetConfig().UnmarshalKey("plugins.haproxy", &haProxyPlugin)
+	if err != nil {
+		return err
+	}
+
+	if haProxyPlugin.Enable != true {
 		return nil
 	}
 
-	host := acmeConfig.GetConfig().GetString("plugins.haproxy.host")
-	port := acmeConfig.GetConfig().GetInt("plugins.haproxy.port")
+	for _, server := range haProxyPlugin.Servers {
+		log.Infof("Working on server %s:%d", server.Host, server.Port)
 
-	socket := tcp_socket.TCPSocket{
-		Address: host,
-		Port:    port,
-	}
+		socket := tcp_socket.TCPSocket{
+			Address: server.Host,
+			Port:    server.Port,
+		}
 
-	// Remove empty lines and trim trailing slashes to prevent HAProxy runtime API from yelling
-	// at us claiming invalid commands. Blank lines and trailing `\n`'s break things...
-	content := strings.TrimSuffix(strings.Replace(contents, "\n\n", "\n", -1), "\n")
+		// Remove empty lines and trim trailing slashes to prevent HAProxy runtime API from yelling
+		// at us claiming invalid commands. Blank lines and trailing `\n`'s break things...
+		content := strings.TrimSuffix(strings.Replace(contents, "\n\n", "\n", -1), "\n")
 
-	commands := []string{
-		"new ssl cert " + certPath,
-		fmt.Sprintf("set ssl cert %s <<\n%s\n", certPath, content),
-		"commit ssl cert " + certPath,
-		fmt.Sprintf("add ssl crt-list %s <<\n%s\n", certListPath, certPath),
-	}
+		commands := []string{
+			"new ssl cert " + certPath,
+			fmt.Sprintf("set ssl cert %s <<\n%s\n", certPath, content),
+			"commit ssl cert " + certPath,
+			fmt.Sprintf("add ssl crt-list %s <<\n%s\n", certListPath, certPath),
+		}
 
-	for i, cmd := range commands {
-		err := socket.Write(cmd)
-		if err != nil {
-			return fmt.Errorf("error running command %d: %v", i, err)
+		for i, cmd := range commands {
+			err := socket.Write(cmd)
+			if err != nil {
+				log.Errorf("error running command %d: %v", i, err)
+				return fmt.Errorf("error running command %d: %v", i, err)
+			}
 		}
 	}
 
